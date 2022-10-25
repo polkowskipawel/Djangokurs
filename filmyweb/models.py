be73ec6f4a88c8d4d8a04b560e30cfa07ca5ce54
Pawel <polkowski.pawel@gmail.com>
2022-10-25 19:12:03 +0200

    Models, baza danych, Fields

--- filmyweb/models.py
+++ filmyweb/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Film(models.Model):
+    tytul = models.CharField(max_length=64, blank=False,unique=True) #blank czy moze byc puste
+    rok = models.PositiveSmallIntegerField(default=2000)
+    opis = models.TextField(default="")
+    premiera = models.DateField(null=True,blank=True)
+    imdb_rating = models.DecimalField(max_digits=4, decimal_places=2,null=True,blank=True)    #10.00
+    plakat = models.ImageField(upload_to="plakaty", null=True, blank=True)
+
+    #https://docs.djangoproject.com/en/4.1/ref/models/fields/
+    def __str__(self):
+        return self.tytul_z_rokiem()
+
+    def tytul_z_rokiem(self):
+        return "{} ({})".format(self.tytul, self.rok)
