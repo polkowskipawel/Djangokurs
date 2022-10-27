aa6fa90eef77e90c7432203599efa9ab9f79b70e
Paweł Polkowski <polkowski.pawel@gmail.com>
2022-10-27 11:55:44 +0200

    Changes to Admin.py and addition of example files to the database

--- filmyweb/admin.py
+++ filmyweb/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from .models import Film
 
 # Register your models here.
-admin.site.register(Film)
+
+#admin.site.register(Film)
+
+@admin.register(Film)
+class FilmAdmin(admin.ModelAdmin):
+    #fields = ["tytul","opis","rok"] # wybieramy co ma byc widoczne w admin
+    #exclude = ["opis"]  #wszystko oprocz opisu
+    list_display = ["tytul","imdb_rating","rok"] #jak widac filmy
+    list_filter = ("rok",) #dodanie filtru
+    search_fields = ("tytul",) #szukajka po tytule
