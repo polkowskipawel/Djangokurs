c033f36b8f0d201c24cfa20c5004a382058b27bb
Paweł Polkowski <polkowski.pawel@gmail.com>
2022-10-27 12:55:47 +0200

    changes in urls.py view page

--- filmy/urls.py
+++ filmy/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 from filmyweb.views import test_response
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("test/",test_response)
+    path("filmy/",include('filmyweb.urls'))
 ]
---
+++ filmyweb/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from filmyweb.views import test_response
+
+urlpatterns = [
+    path("test/",test_response)
+]
